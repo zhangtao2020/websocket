a00cc05916f074897c9de8535ef2243790caa9ab
dswd1656@gmail.com <dswd1656@gmail.com>
2020-09-17 15:01:40 +0800

    11

---
+++ src/main/java/com/chat/controller/SocketController.java
@@ -0,0 +1,27 @@
+package com.chat.controller;
+
+import com.chat.service.WebSocketServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SocketController {
+
+    @Autowired
+    private WebSocketServer webSocketServer;
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/webSocket")
+    public ModelAndView socket() {
+        ModelAndView mav = new ModelAndView("/webSocket");
+//        mav.addObject("userId", userId);
+        return mav;
+    }
+}
